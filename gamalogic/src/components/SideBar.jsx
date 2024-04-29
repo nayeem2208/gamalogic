29b43a246148a8d5167d2cdca2aae77c140d3804
nayeem2208 <nayeem2281998@gmail.com>
2024-04-29 12:05:09 +0530

    new build

--- gamalogic/src/components/SideBar.jsx
+++ gamalogic/src/components/SideBar.jsx
@@ -14,6 +14,7 @@ import { RiProfileLine } from "react-icons/ri";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserState } from "../context/userContext";
+import { startTransition } from 'react';
 
 function SideBar() {
   let [uploadfileDropDown, setUploadFileDropDown] = useState(false);
@@ -30,9 +31,11 @@ function SideBar() {
   };
 
   function logoutHandler() {
+    startTransition(() => {
     localStorage.removeItem("Gamalogic_token");
     setUserDetails(null);
     navigate("/signin");
+  });
 }
 
 
