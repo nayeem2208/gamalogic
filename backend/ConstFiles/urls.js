0383335b83a6be31f287fdaa405d2329d68bc4ec
nayeem2208 <nayeem2281998@gmail.com>
2024-06-21 16:58:39 +0530

    get Api updated

--- backend/ConstFiles/urls.js
+++ backend/ConstFiles/urls.js
@@ -1,7 +1,7 @@
 let urls={
     passwordUrl:'http://service.gamalogic.com',
-    // frontendUrl:'https://app.gamalogic.com'
-    frontendUrl:'https://beta.gamalogic.com'
+    frontendUrl:'https://app.gamalogic.com'
+    // frontendUrl:'https://beta.gamalogic.com'
 
 }
 export default urls
