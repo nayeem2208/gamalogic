2a428a012c203b456ccc566b3096aa7e0989bf14
nayeem2208 <nayeem2281998@gmail.com>
2024-09-12 15:38:01 +0530

    webhook subscription charged management

--- backend/controllers/APIController.js
+++ backend/controllers/APIController.js
@@ -1079,142 +1079,143 @@ let APIControllers = {
 
       let subscriptionDetails = await dbConnection.query(`SELECT * FROM razorpay_subscription Where subscription_id='${subId}'ORDER BY glid DESC 
           LIMIT 1`)
+      if (subscriptionDetails[0][0].length>0) {
+        let userDetails = await dbConnection.query(`SELECT * FROM registration WHERE rowid='${subscriptionDetails[0][0].customer_id}'`)
 
-      let userDetails = await dbConnection.query(`SELECT * FROM registration WHERE rowid='${subscriptionDetails[0][0].customer_id}'`)
+        let planDetails = RazorpayPrice.find(([credit, id, period]) => id == subscriptionDetails[0][0].plan_id)
+        console.log(planDetails, 'plan details')
 
-      let planDetails = RazorpayPrice.find(([credit, id, period]) => id == subscriptionDetails[0][0].plan_id)
-      console.log(planDetails, 'plan details')
-
-      if (event === 'subscription.charged') {
-        const chargeDate = new Date(subscriptionDetails[0][0].timestamp).toISOString().split('T')[0];
-        const today = new Date().toISOString().split('T')[0];
-        const dateMatch = chargeDate != today;
-        console.log(today, chargeDate, 'date of db and now')
-        console.log(dateMatch, 'date match')
+        if (event === 'subscription.charged') {
+          const chargeDate = new Date(subscriptionDetails[0][0].timestamp).toISOString().split('T')[0];
+          const today = new Date().toISOString().split('T')[0];
+          const dateMatch = chargeDate != today;
+          console.log(today, chargeDate, 'date of db and now')
+          console.log(dateMatch, 'date match')
 
 
-        if (dateMatch) {
-          let newBalance = userDetails[0][0].credits + planDetails[0]
+          if (dateMatch) {
+            let newBalance = userDetails[0][0].credits + planDetails[0]
 
-          let paymentId = payload?.payment?.entity?.id
-          console.log(paymentId, 'payement id')
+            let paymentId = payload?.payment?.entity?.id
+            console.log(paymentId, 'payement id')
 
-          var instance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_SECRET })
-          let resp = await instance.payments.fetch(paymentId)
-          console.log(resp, 'resppp')
+            var instance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_SECRET })
+            let resp = await instance.payments.fetch(paymentId)
+            console.log(resp, 'resppp')
 
-          let subscriptinDetails = await instance.subscriptions.fetch(subId)
-          console.log(subscriptinDetails, 'subbbbbb detailssss')
+            let subscriptinDetails = await instance.subscriptions.fetch(subId)
+            console.log(subscriptinDetails, 'subbbbbb detailssss')
 
-          if (!resp) {
-            console.log('error fetching response  ')
-            return res.status(500).json({ error: 'Error fetching payment response' });
-          }
+            if (!resp) {
+              console.log('error fetching response  ')
+              return res.status(500).json({ error: 'Error fetching payment response' });
+            }
 
-          await dbConnection.query(`UPDATE registration SET credits = '${newBalance}' WHERE rowid = '${subscriptionDetails[0][0].customer_id}'`);
-          let content
+            await dbConnection.query(`UPDATE registration SET credits = '${newBalance}' WHERE rowid = '${subscriptionDetails[0][0].customer_id}'`);
+            let content
 
-          let amount = resp.amount / 100
-          let fee = Math.round(resp.fee / 100)
-          let tax = resp.tax / 100
-          const query = `
+            let amount = resp.amount / 100
+            let fee = Math.round(resp.fee / 100)
+            let tax = resp.tax / 100
+            const query = `
           INSERT INTO razorpay_subscription (id, amount,fee,tax, order_id, method, amount_refunded, refund_status, description, card_id, bank, wallet, vpa, email, contact, token_id, notes_address, rrn, upi_transaction_id, created_at, upi_vpa, entity, plan_id, customer_id, status,subscription_id)
           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)
         `;
 
-          const values = [
-            resp.id || null,
-            amount || null,
-            fee || null,
-            tax || null,
-            resp.order_id || null,
-            resp.method || null,
-            resp.amount_refunded || null,
-            resp.refund_status || null,
-            resp.description || null,
-            resp.card_id || null,
-            resp.bank || null,
-            resp.wallet || null,
-            resp.vpa || null,
-            resp.email || null,
-            resp.contact || null,
-            resp.token_id || null,
-            resp.notes?.address || null,
-            resp.acquirer_data?.rrn || null,
-            resp.acquirer_data?.upi_transaction_id || null,
-            new Date(subscriptinDetails.created_at * 1000).toISOString() || null,
-            resp.upi?.vpa || null,
-            subscriptinDetails.entity || null,
-            subscriptinDetails.plan_id || null,
-            userDetails[0][0].rowid,
-            subscriptinDetails.status || null,
-            subscriptinDetails.id || null,
-          ]
+            const values = [
+              resp.id || null,
+              amount || null,
+              fee || null,
+              tax || null,
+              resp.order_id || null,
+              resp.method || null,
+              resp.amount_refunded || null,
+              resp.refund_status || null,
+              resp.description || null,
+              resp.card_id || null,
+              resp.bank || null,
+              resp.wallet || null,
+              resp.vpa || null,
+              resp.email || null,
+              resp.contact || null,
+              resp.token_id || null,
+              resp.notes?.address || null,
+              resp.acquirer_data?.rrn || null,
+              resp.acquirer_data?.upi_transaction_id || null,
+              new Date(subscriptinDetails.created_at * 1000).toISOString() || null,
+              resp.upi?.vpa || null,
+              subscriptinDetails.entity || null,
+              subscriptinDetails.plan_id || null,
+              userDetails[0][0].rowid,
+              subscriptinDetails.status || null,
+              subscriptinDetails.id || null,
+            ]
 
-          await dbConnection.query(query, values);
-          if (planDetails[2] == 'monthly') {
-            content = `
+            await dbConnection.query(query, values);
+            if (planDetails[2] == 'monthly') {
+              content = `
             <p>Your subscription has been renewed successfully. We have processed your payment of $${Number(Math.round(amount)).toLocaleString()} for ${Number(planDetails[0]).toLocaleString()} credits has been successfully processed.</p>
             
             <p>If you have any questions or concerns regarding this payment or your subscription, please feel free to contact us.</p>
             `
+            }
+            else {
+              content = `
+            <p>Your subscription has been renewed successfully. We have processed your payment of $${Number(Math.round(amount)).toLocaleString()} for ${Number(planDetails[0]).toLocaleString()} credits has been successfully processed.</p>
+            
+            <p>If you have any questions or concerns regarding this payment or your subscription, please feel free to contact us.</p>
+            `
+            }
+            let isMonthlyInEmail = planDetails[2] == 'monthly' ? 'Monthly' : 'Annual'
+            sendEmail(
+              userDetails[0][0].username,
+              userDetails[0][0].emailid,
+              `Gamalogic ${isMonthlyInEmail} Subscription Payment successful`,
+              basicTemplate(userDetails[0][0].username, content)
+            );
+
           }
           else {
-            content = `
-            <p>Your subscription has been renewed successfully. We have processed your payment of $${Number(Math.round(amount)).toLocaleString()} for ${Number(planDetails[0]).toLocaleString()} credits has been successfully processed.</p>
-            
-            <p>If you have any questions or concerns regarding this payment or your subscription, please feel free to contact us.</p>
-            `
+            console.log('date is match so updation is not needed')
           }
+
+          // const existingEntryCreationDate = new Date(subscriptionDetails[0][0].start_time).toISOString().split('T')[0]; // Extract date part
+
+        }
+        else if (event === 'subscription.cancelled') {
           let isMonthlyInEmail = planDetails[2] == 'monthly' ? 'Monthly' : 'Annual'
-          sendEmail(
-            userDetails[0][0].username,
-            userDetails[0][0].emailid,
-            `Gamalogic ${isMonthlyInEmail} Subscription Payment successful`,
-            basicTemplate(userDetails[0][0].username, content)
-          );
-
-        }
-        else{
-          console.log('date is match so updation is not needed')
-        }
-
-        // const existingEntryCreationDate = new Date(subscriptionDetails[0][0].start_time).toISOString().split('T')[0]; // Extract date part
-
-      }
-      else if (event === 'subscription.cancelled') {
-        let isMonthlyInEmail = planDetails[2] == 'monthly' ? 'Monthly' : 'Annual'
-        let content
-        if (isMonthlyInEmail) {
-          content = `
+          let content
+          if (isMonthlyInEmail) {
+            content = `
         <p>We're sorry to see you go! Your monthly subscription has been successfully cancelled.</p>
         
         <p>If you have any questions or need assistance with your account, please don't hesitate to reach out.</p>
 
         <p>Thank you for choosing us, and we hope to serve you again in the future!</p>
         `
-        } else {
-          content = `
+          } else {
+            content = `
         <p>We're sorry to see you go! Your annual subscription has been successfully cancelled.</p>
         
         <p>If you have any questions or need assistance with your account, please don't hesitate to reach out.</p>
 
         <p>Thank you for choosing us, and we hope to serve you again in the future!</p>
         `
-        }
-        sendEmail(
-          userDetails[0][0].username,
-          userDetails[0][0].emailid,
-          `Gamalogic ${isMonthlyInEmail} Subscription Cancellation`,
-          basicTemplate(userDetails[0][0].username, content)
-        );
-        let stopTime = new Date().toISOString()
+          }
+          sendEmail(
+            userDetails[0][0].username,
+            userDetails[0][0].emailid,
+            `Gamalogic ${isMonthlyInEmail} Subscription Cancellation`,
+            basicTemplate(userDetails[0][0].username, content)
+          );
+          let stopTime = new Date().toISOString()
 
-        await dbConnection.query(
-          `UPDATE registration SET is_monthly = 0, is_annual = 0,is_active=0, subscription_stop_time = ? WHERE rowid = ?`,
-          [stopTime, userDetails[0][0].rowid]
-        );
-        console.log('subscription cancelled succesfully.............')
+          await dbConnection.query(
+            `UPDATE registration SET is_monthly = 0, is_annual = 0,is_active=0, subscription_stop_time = ? WHERE rowid = ?`,
+            [stopTime, userDetails[0][0].rowid]
+          );
+          console.log('subscription cancelled succesfully.............')
+        }
       }
       res.status(200).send('Webhook processed successfully');
 
