fc5afb25b128e070b2075e3817df30f81a46cdda
nayeem2208 <nayeem2281998@gmail.com>
2024-09-26 16:27:53 +0530

    adding purchse data through razorpay

--- backend/utils/INRtoUSD.js
+++ backend/utils/INRtoUSD.js
@@ -1,11 +1,20 @@
-import { Convert } from "easy-currencies";
-
+import axios from "axios";
 
 export default async function InrToUsdConverter(amount) {
-  try {
-    const value = await Convert(amount).from("INR").to("USD");
-    return value;
-  } catch (error) {
-    console.error('Error converting INR to USD:', error);
-  }
+    try {
+        const response=await axios('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/inr.json')
+        console.log(response, 'resppp')
+
+        const exchangeRate = response.data.inr.usd;
+
+        if (!exchangeRate) {
+            throw new Error(`Invalid target currency: ${targetCurrency}`);
+        }
+
+        const convertedAmount = amount * exchangeRate;
+        return convertedAmount;
+    } catch (error) {
+        console.error('Error converting INR to USD:', error);
+
+    }
 }
