a4dd46405e5b9f96e5553924b61c810c7d06dd2b
nayeem2208 <nayeem2281998@gmail.com>
2024-11-22 18:40:00 +0530

    loyalty webhook middleware

--- backend/controllers/APIController.js
+++ backend/controllers/APIController.js
@@ -1518,7 +1518,7 @@ let APIControllers = {
   },
   loyalityWebhook: async (req, res) => {
     try {
-      console.log(req.body,req)
+      console.log(req.body)
       res.status(200).json({message:'success'})
     } catch (error) {
       res.status(500)
